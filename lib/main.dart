1f52c7573cabbda86e95ae9c0b9606ff9a0ff3d8
MohammedGhazaly <warendragon67@gmail.com>
2023-10-08 11:05:50 +0200

    Finished splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Future.delayed(const Duration(seconds: 2));
+  FlutterNativeSplash.remove();
+
   runApp(const NewsApp());
 }
 
