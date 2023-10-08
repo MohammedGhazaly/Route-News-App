4e9bd128ab842582ac91f4509bd3da40dc583daf
MohammedGhazaly <warendragon67@gmail.com>
2023-10-08 10:54:03 +0200

    Initial app

---
+++ README.md
@@ -0,0 +1,2 @@
+# Route News App
+News app using Flutter and the Api provided from [News Api](https://newsapi.org/).
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const NewsApp());
+}
+
+class NewsApp extends StatelessWidget {
+  const NewsApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'News App',
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+    );
+  }
+}
